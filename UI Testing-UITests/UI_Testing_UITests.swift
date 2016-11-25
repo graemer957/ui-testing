d3e4e4d2f9bbf546c7577f731fb3d55ad1d840d5
Graeme Read <graeme@sigma957.net>
2016-11-25 22:34:56 +0000

    Force screenshot to be taken after initial setup

--- UI Testing-UITests/UI_Testing_UITests.swift
+++ UI Testing-UITests/UI_Testing_UITests.swift
@@ -23,6 +23,7 @@ class UI_Testing_UITests: XCTestCase {
         XCUIApplication().launch()
         
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        XCUIDevice.shared().orientation = .unknown
     }
     
     override func tearDown() {

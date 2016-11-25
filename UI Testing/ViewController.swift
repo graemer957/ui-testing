1cf0d357202e8e5311f93498cc766fda229e9da5
Graeme Read <graeme@sigma957.net>
2016-11-25 21:30:37 +0000

    Add some very basic functionality

--- UI Testing/ViewController.swift
+++ UI Testing/ViewController.swift
@@ -11,9 +11,22 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // MARK: - Properties
+    @IBOutlet private weak var label: UILabel!
+    @IBOutlet private weak var spinner: UIActivityIndicatorView!
+    
+    
     // MARK: - UIViewController
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    
+    // MARK: - IBActions
+    @IBAction
+    private func doSomething() {
+        label.text = "Hello world"
+    }
+    
     
 }

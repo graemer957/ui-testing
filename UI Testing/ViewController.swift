cf6d4b634e36495a0d919ce960bdfa9ec14e65b5
Graeme Read <graeme@sigma957.net>
2016-11-25 21:19:56 +0000

    Create base Xcode project

---
+++ UI Testing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UI Testing
+//
+//  Created by Graeme Read on 25/11/2016.
+//  Copyright © 2016 Graeme Read. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
